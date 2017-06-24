28e8911c54cb1ca71098e08608d67709fcfdb858
Chatri Sae-Tung <gearmag@gmail.com>
2017-06-24 20:10:01 +0700

    Revert "add material and hammerjs."
    This reverts commit 6b553d220266f83d9741a31259c273d556070e22.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
-import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -14,12 +12,8 @@ import 'hammerjs';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    BrowserAnimationsModule,
-    MdButtonModule,
-    MdCheckboxModule
+    HttpModule
   ],
-  exports: [MdButtonModule, MdCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
