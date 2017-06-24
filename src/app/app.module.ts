66b56afe2c3007d54d9cbef8b384a5c236d1fc3d
Chatri Sae-Tung <gearmag@gmail.com>
2017-06-24 20:41:21 +0700

    add material and hammerjs back.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import 'hammerjs';
+
 
 @NgModule({
   declarations: [
@@ -12,8 +17,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule
   ],
+  exports: [MdButtonModule, MdCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
